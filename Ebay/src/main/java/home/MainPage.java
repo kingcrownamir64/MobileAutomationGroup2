a839a04ca51d0f26eba3c180391c47a6f754f9e5
kingcrownamir64 <amirhafiz08@gmail.com>
2019-04-04 04:36:58 -0400

    Added 23 test cases, completed date/keyword driven

--- Ebay/src/main/java/home/MainPage.java
+++ Ebay/src/main/java/home/MainPage.java
@@ -52,9 +52,51 @@ public class MainPage extends MobileAPI {
     @FindBy(id = "com.ebay.mobile:id/search_src_text")
     public WebElement SearchBox;
 
+    @FindBy(id = "com.ebay.mobile:id/menu_search")
+    public WebElement SearchButton;
+
+    @FindBy(id = "com.ebay.mobile:id/menuitem_home")
+    public WebElement HomeButton;
+
+    @FindBy(id = "com.ebay.mobile:id/menuitem_notifications")
+    public WebElement NotificationsButton;
+
+    @FindBy(id = "com.ebay.mobile:id/menuitem_messages")
+    public WebElement MessagesButton;
+
+    @FindBy(id = "com.ebay.mobile:id/menuitem_watching")
+    public WebElement WatchingButton;
+
+    @FindBy(id = "com.ebay.mobile:id/menuitem_saved")
+    public WebElement SavedButton2;
+
+    @FindBy(id = "com.ebay.mobile:id/menuitem_purchases")
+    public WebElement PurchaseButton;
+
+    @FindBy(id = "com.ebay.mobile:id/menuitem_bids_offers")
+    public WebElement Bids_OfferButton;
+
+    @FindBy(id = "com.ebay.mobile:id/menuitem_selling")
+    public WebElement SellingButton2;
+
+    @FindBy(id = "com.ebay.mobile:id/menuitem_categories")
+    public WebElement CategoriesButton2;
+
+    @FindBy(id = "com.ebay.mobile:id/menuitem_deals")
+    public WebElement DealsButton2;
+
+    @FindBy(id = "com.ebay.mobile:id/menuitem_settings")
+    public WebElement SettingButton;
+
+    @FindBy(id = "com.ebay.mobile:id/menuitem_help_contact")
+    public WebElement HelpButton;
 
 
 
+    public void setSearchButton() throws Exception {
+        SearchButton.click();
+        Thread.sleep(1000);
+    }
 
     public void setClickMenu() throws Exception {
         btnBurger.click();
@@ -112,11 +154,110 @@ public class MainPage extends MobileAPI {
     public void searchForItems(String item) throws Exception {
         SearchBox.clear();
         Thread.sleep(2000);
-        SearchBox.sendKeys(item);
+        SearchBox.sendKeys(item + "\n");
+//        SearchBox.sendKeys(item);
         Thread.sleep(2000);
+        SearchButton.click();
     }
     public void setSearchBoxButton() throws Exception {
         SearchBoxButton.click();
         Thread.sleep(4000);
+    }
+
+    public void setMenuHomeButton() throws Exception {
+        btnBurger.click();
+        Thread.sleep(4000);
+        HomeButton.isDisplayed();
+        Assert.assertTrue(true);
+        HomeButton.click();
+        Thread.sleep(1000);
+    }
+
+    public void setMenuNotificationsButton() throws Exception {
+        btnBurger.click();
+        NotificationsButton.isDisplayed();
+        Assert.assertTrue(true);
+        NotificationsButton.click();
+        Thread.sleep(1000);
+    }
+
+    public void setMenuMessagesButton() throws Exception {
+        btnBurger.click();
+        MessagesButton.isDisplayed();
+        Assert.assertTrue(true);
+        MessagesButton.click();
+        Thread.sleep(1000);
+    }
+
+    public void setMenuWatchingButton() throws Exception {
+        btnBurger.click();
+        WatchingButton.isDisplayed();
+        Assert.assertTrue(true);
+        WatchingButton.click();
+        Thread.sleep(1000);
+    }
+
+    public void setMenuSavedButton2() throws Exception {
+        btnBurger.click();
+        SavedButton2.isDisplayed();
+        Assert.assertTrue(true);
+        SavedButton2.click();
+        Thread.sleep(1000);
+    }
+
+    public void setMenuPurchasesButton() throws Exception {
+        btnBurger.click();
+        PurchaseButton.isDisplayed();
+        Assert.assertTrue(true);
+        PurchaseButton.click();
+        Thread.sleep(1000);
+    }
+
+    public void setMenuBids_OffersButton() throws Exception {
+        btnBurger.click();
+        Bids_OfferButton.isDisplayed();
+        Assert.assertTrue(true);
+        Bids_OfferButton.click();
+        Thread.sleep(1000);
+    }
+
+    public void setMenuSellingButton() throws Exception {
+        btnBurger.click();
+        SellingButton2.isDisplayed();
+        Assert.assertTrue(true);
+        SellingButton2.click();
+        Thread.sleep(1000);
+    }
+
+    public void setMenuCategoriesButton() throws Exception {
+        btnBurger.click();
+        CategoriesButton2.isDisplayed();
+        Assert.assertTrue(true);
+        CategoriesButton2.click();
+        Thread.sleep(1000);
+    }
+
+    public void setMenuDealsButton() throws Exception {
+        btnBurger.click();
+        DealsButton2.isDisplayed();
+        Assert.assertTrue(true);
+        DealsButton2.click();
+        Thread.sleep(1000);
+    }
+
+    public void setMenuSettingButton() throws Exception {
+        btnBurger.click();
+        SettingButton.isDisplayed();
+        Assert.assertTrue(true);
+        SettingButton.click();
+        Thread.sleep(1000);
+    }
+
+    public void setMenuHelpButton() throws Exception {
+        btnBurger.click();
+        HelpButton.isDisplayed();
+        Assert.assertTrue(true);
+        HelpButton.click();
+        Thread.sleep(1000);
     }
 }
--- Ebay/src/test/java/testHome/TestMainPage.java
+++ Ebay/src/test/java/testHome/TestMainPage.java
@@ -1,5 +1,6 @@
 package testHome;
 
+import com.sun.javaws.exceptions.InvalidArgumentException;
 import common.MobileAPI;
 import datasource.MySqlData;
 import home.MainPage;
@@ -7,6 +8,7 @@ import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import reporting.TestLogger;
+import utility.DataReader;
 
 import java.util.List;
 
@@ -14,7 +16,11 @@ import static datasource.MySqlData.convertToString;
 
 public class TestMainPage extends MobileAPI{
     MySqlData mySqlData;
-MainPage mainPage;
+    MainPage mainPage;
+
+    DataReader excelData = new DataReader();
+    String path ="/Users/amir/Desktop/MobileAutomationGroup2/Ebay/src/test/resources/ExcelFile.xls";
+
 
     @BeforeMethod
     public void initialize() throws Exception {
@@ -24,53 +30,53 @@ MainPage mainPage;
     }
 
 
-//    @Test(priority = 1)
-//    public void testMenu() throws Exception {
-//        mainPage.setClickMenu();
-//    }
-//
-//    @Test(priority = 2)
-//    public void testSellingButton() throws Exception {
-//        mainPage.setClickSellingButton();
-//    }
-//
-//    @Test(priority = 3)
-//    public void testDealsButton() throws Exception {
-//        mainPage.setClickDealsButton();
-//    }
-//
-//    @Test(priority = 4)
-//    public void testCategoriesButton() throws Exception {
-//        mainPage.setClickCategoriesButton();
-//    }
-//
-//    @Test(priority = 5)
-//    public void testSavedButton() throws Exception {
-//        mainPage.setClickSavedButton();
-//    }
+    @Test(priority = 1)
+    public void testMenu() throws Exception {
+        mainPage.setClickMenu();
+    }
 
-//    @Test(priority = 6)
-//    public void testRegistrationButton() throws Exception {
-//        mainPage.setClickRegisterButton();
-//    }
+    @Test(priority = 2)
+    public void testSellingButton() throws Exception {
+        mainPage.setClickSellingButton();
+    }
 
-//    @Test(priority = 7)
-//    public void testSignInButton() throws Exception {
-//        mainPage.setClickSignInButton();
-//    }
+    @Test(priority = 3)
+    public void testDealsButton() throws Exception {
+        mainPage.setClickDealsButton();
+    }
 
-//    @Test(priority = 8)
-//    public void testEbayLogo() throws Exception {
-//        mainPage.setEbayLogo();
-//    }
+    @Test(priority = 4)
+    public void testCategoriesButton() throws Exception {
+        mainPage.setClickCategoriesButton();
+    }
 
-//    @Test(priority = 9)
-//    public void testSeeDetailsButton() throws Exception {
-//        mainPage.setDetailsButton();
-//    }
+    @Test(priority = 5)
+    public void testSavedButton() throws Exception {
+        mainPage.setClickSavedButton();
+    }
 
-    //DATABASE IMPLEMENTED
-    @Test
+    @Test(priority = 6)
+    public void testRegistrationButton() throws Exception {
+        mainPage.setClickRegisterButton();
+    }
+
+    @Test(priority = 7)
+    public void testSignInButton() throws Exception {
+        mainPage.setClickSignInButton();
+    }
+
+    @Test(priority = 8)
+    public void testEbayLogo() throws Exception {
+        mainPage.setEbayLogo();
+    }
+
+    @Test(priority = 9)
+    public void testSeeDetailsButton() throws Exception {
+        mainPage.setDetailsButton();
+    }
+
+//    DATABASE IMPLEMENTED
+    @Test(priority = 10)
     public void testSearchBarUsingDatabase() throws Exception {
         mainPage.setSearchBoxButton();
         List<String> list = null;
@@ -84,4 +90,82 @@ MainPage mainPage;
         }
     }
 
+//    KEYWORD DRIVEN TEST
+    @Test(priority = 11)
+    public void testSearchAndNavigateUsingKeywordDriven() throws Exception {
+        String[] testSteps = excelData.fileReader3(path, 0);
+        for (String str : testSteps) {
+            switch (str) {
+                case "search":
+                    testMenu();
+                    break;
+                case "navigate":
+                    testSellingButton();
+                    break;
+                default:
+                    throw new Exception("Invalid choice");
+            }
+            System.out.println(str);
+        }
+    }
+
+    @Test(priority = 12)
+    public void testMenuHomeButton() throws Exception {
+        mainPage.setMenuHomeButton();
+    }
+
+    @Test(priority = 13)
+    public void testMenuNotificationsButton() throws Exception {
+        mainPage.setMenuNotificationsButton();
+    }
+
+    @Test(priority = 14)
+    public void testMenuMessagesButton() throws Exception {
+        mainPage.setMenuMessagesButton();
+    }
+
+    @Test(priority = 15)
+    public void testMenuWatchingButton() throws Exception {
+        mainPage.setMenuWatchingButton();
+    }
+
+    @Test(priority = 16)
+    public void testMenuSavedButton() throws Exception {
+        mainPage.setMenuSavedButton2();
+    }
+
+    @Test(priority = 17)
+    public void testMenuPurchasesButton() throws Exception {
+        mainPage.setMenuPurchasesButton();
+    }
+
+    @Test(priority = 18)
+    public void testMenuBid_OffersButton() throws Exception {
+        mainPage.setMenuBids_OffersButton();
+    }
+
+    @Test(priority = 19)
+    public void testMenuSellingButton() throws Exception {
+        mainPage.setMenuSellingButton();
+    }
+
+    @Test(priority = 20)
+    public void testMenuCategoriesButton() throws Exception {
+        mainPage.setMenuCategoriesButton();
+    }
+
+    @Test(priority = 21)
+    public void testMenuDealsButton() throws Exception {
+        mainPage.setMenuDealsButton();
+    }
+
+    @Test(priority = 22)
+    public void testMenuSettingButton() throws Exception {
+        mainPage.setMenuSettingButton();
+    }
+
+    @Test(priority = 23)
+    public void testMenuHelpButton() throws Exception {
+        mainPage.setMenuHelpButton();
+    }
 }
